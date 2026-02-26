12301bee18868f61c3616dbe282ddcfdcdd24993
Makrina <makrinaemad51@gmail.com>
2026-02-26 14:37:41 +0200

    Jelly & Angular Tasks

---
+++ Jelly/src/main/java/SimpleFunctions.java
@@ -0,0 +1,12 @@
+package com.example;
+
+public class SimpleFunctions {
+    
+    public String checkEvenOrOdd(int n) {
+    if (n % 2 == 0) {
+        return "Even";
+    } else {
+        return "Odd";
+    }
+}
+}
